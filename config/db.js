b701281ff4c57b1c827ff6d8d023e895874b8e95
kamula <kamulaisaac@gmail.com>
2021-03-03 01:57:38 +0300

    db connection and error handling

--- config/db.js
+++ config/db.js
@@ -1,11 +1,22 @@
 const sequelize = require('sequelize')
 
 const connectDB = async() => {
-    const { DB_NAME, USERNAME, PASSWORD } = process.env
-    const conn = await new sequelize(DB_NAME, USERNAME, PASSWORD, {
+    const { DB_NAME, USER_NAME, PASSWORD } = process.env
+
+
+    // const url = `postgres://${USER_NAME}:${PASSWORD}@127.0.0.1:5432/${DB_NAME}`.replace(/\s/g, '')
+    // const conn = await new sequelize(url)
+    const conn = await new sequelize(DB_NAME, USER_NAME, PASSWORD, {
         host: 'localhost',
         dialect: 'postgres'
-    })
-    console.log(`database connected successfully`.blue.underline)
+    });
+
+    try {
+        await conn.authenticate()
+        console.log(`database connected successfully`.blue.underline)
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+
+    }
 }
 module.exports = connectDB
